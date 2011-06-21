e81ad180564a78232e2078feb97d23d524ba29e5
Bob Browning <bob.browning@pressassociation.com>
2011-06-21 13:07:12 +0100

    Added restart waiting period, runs check once per second for 10 seconds until server is ready

--- src/main/java/com/marklogic/maven/InstallMojo.java
+++ src/main/java/com/marklogic/maven/InstallMojo.java
@@ -76,6 +76,44 @@ public class InstallMojo extends AbstractInstallMojo {
         }
 
         executeAction(ACTION_RESTART);
+
+        /**
+         * Ensure server is ready
+         */
+        int count = 10;
+        boolean success = false;
+        RequestException lastException = null;
+        while(count-- > 0) {
+            /* Try and get session */
+            Session session = getXccSession();
+            if(session != null) {
+                /* Attempt simple xquery */
+                AdhocQuery q = session.newAdhocQuery("xquery version \"1.0-ml\";\n1");
+                try {
+                    session.submitRequest(q);
+                    success = true;
+                } catch (RequestException e) {
+                    lastException = e;
+                } finally {
+                    session.close();
+                }
+            }
+
+            if(success) {
+                break;
+            } else {
+                try {
+                    getLog().info("Waiting for server to be ready.");
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    // Interrupted
+                }
+            }
+        }
+
+        if(!success) {
+            throw new MojoExecutionException("Job timed out waiting for servers on host to restart.", lastException);
+        }
     }
 
     private void installPipeline(ResourceFileSet[] resources) {
